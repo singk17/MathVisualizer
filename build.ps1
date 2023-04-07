3101386ac1e8afb5147870520bcaec7779b166c3
KamS04 <singhkam07@gmail.com>
2023-04-07 16:05:01 -0400

    FSD

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
-elm make src/Main.elm --output=site/index.html
+elm make src/Main.elm --output=docs/index.html
 
-cp site/index.html site/graph.html
-cp site/index.html site/home.html
-cp site/index.html site/help.html
-cp site/index.html site/resources.html
+cp docs/index.html docs/graph.html
+cp docs/index.html docs/home.html
+cp docs/index.html docs/help.html
+cp docs/index.html docs/resources.html
